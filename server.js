b83fe326d692abea9c9ef12b4f6778b632894f70
thayronf3 <thayronf3@gmail.com>
2020-05-27 21:35:51 -0300

    Nesse commit foi configurado a rotas da aplicação usando o / para o local host e o /livros apara um *h1* Listagem de livors. Foi chamado o metodo expresse passando uma referencia para a const app, que por sua vez chama o metodo .get que levanta o serviço

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
+const express = require('express');
+const app = express();
+app.listen(3030, function(){
+    console.log('Servidor rodando na porta');
+});
+
+app.get('/',function(req, resp){
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <body>
+                <h1>Casa do codigo </h1>
+            
+            </body>
+        </html>`)
+});
+
+app.get('/livros',function(req, resp){
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <body>
+                <h1>Listagem de liros</h1>
+            
+            </body>
+        </html>`)
+});
+
+
+
+
+
+
+/*
 const http = require('http');
 
 const servidor = http.createServer(function(req, respost){
+
+    let html = '';
     respost.end(`
         <html>
             <head>
@@ -17,3 +57,4 @@ const servidor = http.createServer(function(req, respost){
 
 servidor.listen(8080);
 
+*/
